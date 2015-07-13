4cace3ff54c477d58529ee30321d71acb4a6cf31
Frieder Bluemle <frieder.bluemle@gmail.com>
2015-07-13 15:16:58 +0800

    Improve up-down command search

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -115,3 +115,12 @@ else
     fi
   fi
 fi
+
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
