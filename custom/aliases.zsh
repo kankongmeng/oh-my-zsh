9e12254d05403a3fa925cdaf510247fbff117394
Frieder Bluemle <frieder.bluemle@gmail.com>
2015-12-04 20:13:42 +0800

    Add aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -56,25 +56,35 @@ alias gds='git diff --summary --stat'
 alias gdcs='git diff --summary --stat --cached'
 alias gdchk='git diff --check 4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 alias gcan='git commit --amend --no-edit'
+alias gcic='git commit -m"Initial commit"'
 alias gf='git fetch'
+alias gfa='gf --all --prune'
+alias gbr='git branch -vv'
+alias gfo='git fork && git remote rename friederbluemle fb'
 alias gg='git grep -I -n --heading --break'
 alias gm='git merge --ff-only'
 alias gst='git stash'
+alias gstrq='git stash && GIT_SEQUENCE_EDITOR=: git rebase --autosquash -i origin/master && git stash pop'
+alias grq='GIT_SEQUENCE_EDITOR=: git rebase --autosquash -i origin/master'
 alias grc='git rebase --continue'
 alias gsp='git stash pop'
 alias gcm='git checkout master'
 alias gcd='git checkout develop'
 alias gl='git log'
 alias gll='git log --format=fuller'
+alias gl1='gll -1'
 alias gcl='git clone'
 alias gcp='git cherry-pick'
 alias gr='git log --graph --full-history --color --date=short --pretty=format:"%x1b[31m%h%x09%x1b[30;1m%ad%x1b[0m%x1b[32m%d%x1b[0m%x20%s%x20%x1b[34;1m[%aE]"'
+alias gr1='gr -10'
 alias gra='gr --all'
 alias grt='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
 alias gh="git remote -v | grep --color=never origin | head -n 1 | grep --color=never -o 'github.*' | sed 's/.git\ .*//' | sed 's/\:/\//' | sed 's/^/https:\/\//' | xargs google-chrome"
 
 alias ackandsdk="ack -i 'com\.android\.tools\.build|buildtools|build\-tools|compilesdk|targetsdk|target=|android-'"
+alias aackandsdk="a -i 'com\.android\.tools\.build|buildtools|build\-tools|compilesdk|targetsdk|target=|android-'"
 alias cpsshpub="xclip -sel clip < ~/.ssh/id_rsa.pub"
+alias aaa="a '<<<' && F 1"
 
 # p2p
 alias upp='adb uninstall com.paypal.android.p2pmobile'
